d1429b8db1acfa53a7694e28f42bb0a920f82d51
Unknown <giacomo1988@hotmail.com>
2018-07-17 07:49:46 +0200

    Added connection timeout

--- src/main/java/jsonify/core/JsonGet.java
+++ src/main/java/jsonify/core/JsonGet.java
@@ -8,19 +8,35 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
+import jsonify.utils.Settings;
+import jsonify.utils.SettingsSingleton;
+
 public class JsonGet {
 	private final static Logger logger = Logger.getLogger(JsonGet.class);
+	private static Settings settings;
 
 	public static String sendRequest(String url) throws ClientProtocolException, IOException {
+		settings = SettingsSingleton.getInstance();
+		
+		Integer timeOut = Integer.parseInt(settings.getProperty("TIMEOUT"));
 		String responseString = new String();
+		
+		RequestConfig.Builder requestBuilder = RequestConfig.custom();
+		requestBuilder = requestBuilder.setConnectTimeout(timeOut);
+		requestBuilder = requestBuilder.setSocketTimeout(timeOut);
+		requestBuilder = requestBuilder.setConnectionRequestTimeout(timeOut);
 
-		HttpClient client = HttpClientBuilder.create().build();
+		HttpClientBuilder builder = HttpClientBuilder.create();
+		builder.setDefaultRequestConfig(requestBuilder.build());
+		
+		HttpClient client = builder.build();
 
 		HttpUriRequest request = new HttpGet(url);
 		request.addHeader(HttpHeaders.ACCEPT, "application/json");
